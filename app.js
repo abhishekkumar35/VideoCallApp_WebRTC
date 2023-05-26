ce5b468fa9288445a8ac075e230fa524c1753ca1
abhishek kumar <flashtoak@gmail.com>
2023-05-27 00:33:36 +0530

    ui v1

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const http = require("http");
+// const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+const io = require("socket.io")(server);
 
-app.use(express.json());
+app.use(express.static("public"));
+let connectedPeers = [];
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname, "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user " + socket.id + " connected to socketio server");
+  connectedPeers.push(socket.id);
+  console.log(connectedPeers);
+  socket.on("disconnect", () => {
+    console.log("user " + socket.id + " disconnected from socketio server");
+    const newConnectedPeers = connectedPeers.filter((peerSocketId) => {
+      peerSocketId !== socket.id;
+    });
+    connectedPeers = newConnectedPeers;
+  });
 });
 
 server.listen(PORT, (err) => {
@@ -17,5 +33,5 @@ server.listen(PORT, (err) => {
     console.log(err);
     return;
   }
-  console.log("Listening at ", PORT);
+  console.log("Listening on Port", PORT);
 });
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+const socket = io("/");
+socket.on("connect", () => {
+  console.log("Successfully connected to wss server");
+  console.log(socket.id);
+});
